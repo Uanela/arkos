57f29cb9574fff8c10f9908bb4fcb42c238b7cc4
uanela <uanelaluiswayne@gmail.com>
2025-07-26 19:09:53 +0200

    chore(arkos): fixing esm/cjs problems

--- packages/arkos/src/utils/cli/utils/cli.helpers.ts
+++ packages/arkos/src/utils/cli/utils/cli.helpers.ts
@@ -1,9 +1,10 @@
 import fs from "fs";
 import { killDevelopmentServerChildProcess } from "../dev";
 import { killProductionServerChildProcess } from "../start";
-import path from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { dirname } from "path";
 
 export function ensureDirectoryExists(dirPath: string): void {
   if (!fs.existsSync(dirPath)) {
@@ -16,22 +17,35 @@ export function killServerChildProcess() {
   killProductionServerChildProcess();
 }
 
-export function getVersion() {
-  let currentDir: string;
-  // @ts-ignore
-  if (typeof __dirname !== "undefined") {
-    // @ts-ignore
-    currentDir = __dirname;
-  } else {
-    // @ts-ignore
-    currentDir = dirname(fileURLToPath((import.meta as any).url));
-  }
+// export function getVersion() {
+//   let currentDir: string;
+//   // @ts-ignore
+//   if (typeof __dirname !== "undefined") {
+//     // @ts-ignore
+//     currentDir = __dirname;
+//   } else {
+//     // @ts-ignore
+//     currentDir = dirname(fileURLToPath((import.meta as any).url));
+//   }
 
-  const packageJson = JSON.parse(
-    fs.readFileSync(
-      path.join(currentDir, "../../../../../package.json"),
-      "utf8"
-    ) || "{}"
-  );
-  return packageJson?.version || "1.0.0";
+//   const packageJson = JSON.parse(
+//     fs.readFileSync(
+//       path.join(currentDir, "../../../../../package.json"),
+//       "utf8"
+//     ) || "{}"
+//   );
+//   return packageJson?.version || "1.0.0";
+// }
+
+//export async function getVersion() {
+//  //@ts-ignore
+//  const pkg = await import("../../../../../package.json", {
+//    with: { type: "json" }, // Use 'with' instead of 'assert'
+//  });
+//  const version = "default" in pkg ? pkg.default.versoin : pkg.version;
+
+//  return version || "1.0.0";
+//}
+export function getVersion() {
+  return "{{version}}";
 }
