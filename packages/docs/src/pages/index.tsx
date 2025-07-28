34b66ccae594416c1e3b794793f729f35ef33bf0
uanela <uanelaluiswayne@gmail.com>
2025-07-28 12:43:26 +0200

    chore(docs): adding npx create-arkos button

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -10,10 +10,7 @@ import { arkosFeatures } from "../utils/arkos-features";
 export default function Home() {
   return (
     <div className="tailwind overflow-hidden max-w-[100vw]">
-      <Layout
-        // title="Arkos"
-        description="Backend framework for simplifying API development with automatic route generation, authentication, and minimal configuration."
-      >
+      <Layout description="Node.js backend framework for simplifying API development with automatic route generation, authentication, and minimal configuration.">
         {/* Hero Section */}
         <header
           id="tailwind"
@@ -23,7 +20,7 @@ export default function Home() {
             {/* Hero Content */}
             <div
               id="tailwind"
-              className="max-w-6xl mx-auto text-center md:mb-32 lg:mt-32 md:mt-20 mt-10 relative h-[100%] md:max-h-[800px]"
+              className="max-w-6xl mx-auto text-center md:mb-24 lg:mt-24 md:mt-20 mt-10 relative h-[100%] md:max-h-[800px]"
             >
               <div className="absolute md:size-[500px] size-[200px] bg-sky-500 rounded-full opacity-30 blur-3xl z-0"></div>
               <div className="absolute bottom-full left-full md:size-[500px] size-[200px] bg-sky-500 rounded-full opacity-50 blur-3xl"></div>
@@ -45,7 +42,7 @@ export default function Home() {
                 , allowing developers to focus on what really matters for the
                 business
               </p>
-              <div className="flex justify-center  z-10 flex-col items-center gap-2 md:flex-row">
+              <div className="flex justify-center md:mt-16 z-10 flex-col items-center gap-2 md:flex-row">
                 <Link
                   to="/docs/intro"
                   className="bg-sky-500 hover:bg-sky-600 text-white font-medium py-2 px-6 rounded-md w-[200px] md:w-[180px] flex items-center gap-2 justify-center z-10"
@@ -61,55 +58,34 @@ export default function Home() {
                   <FontAwesomeIcon icon={faGithub} className="size-5" />
                 </Link>
               </div>
-            </div>
-
-            {/* Installation Command */}
-            {/* <div className="max-w-xl mx-auto mb-16"> */}
-            {/* <div className="bg-gray-800 rounded-md px-4 py-2 flex items-center">
-              <code className="text-gray-300 flex-grow">
-                $ npx create-arkos-api
-              </code>
-              <button className="text-gray-400 hover:text-white">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
+              <div className="mt-3 text-center">
+                <div
+                  className="rounded-md bg-sky-500/10 px-3 py-2.5 max-w-[370px] mx-auto relative group cursor-pointer"
+                  onClick={() =>
+                    navigator.clipboard.writeText("npx create-arkos@latest")
+                  }
                 >
-                  <path d="M8 2a1 1 0 000 2h2a1 1 0 100-2H8z" />
-                  <path d="M3 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v6h-4.586l1.293-1.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L10.414 13H15v3a2 2 0 01-2 2H5a2 2 0 01-2-2V5zM15 11h2a1 1 0 110 2h-2v-2z" />
-                </svg>
-              </button>
+                  <p className="text-sky-100 py-0 my-0 font-mono text-sm">
+                    npx create-arkos@latest
+                  </p>
+                  <div className="absolute right-3 top-[55%] transform -translate-y-1/2  group-hover:opacity-100 transition-opacity text-sky-100">
+                    <svg
+                      className="w-4 h-4 text-gray-400"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-
-          <div className="max-w-2xl mx-auto mb-16">
-            <div className="bg-gray-800 rounded-md p-6">
-              <pre className="text-gray-300">
-                <code>
-                  <span className="text-purple-400">import</span> arkos{" "}
-                  <span className="text-purple-400">from</span>{" "}
-                  <span className="text-green-400">"arkos"</span>;
-                  <br />
-                  arkos.<span className="text-yellow-400">init</span>({"{"}{" "}
-                  <span className="text-blue-400">port</span>:{" "}
-                  <span className="text-orange-400">3000</span> {"}"});
-                </code>
-              </pre>
-            </div>
-          </div>
-
-          <div className="flex flex-col items-center justify-center mb-16">
-            <div className="w-16 h-16 rounded-full overflow-hidden mb-2">
-              <img
-                src="/img/creator.jpg"
-                alt="Creator"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <h3 className="text-lg font-semibold">UANELA COMO</h3>
-            <p className="text-sm text-gray-400">The creator</p>
-          </div> */}
           </div>
         </header>
 
@@ -121,7 +97,7 @@ export default function Home() {
           <div className="container mx-auto relative max-w-6xl">
             <div className="absolute top-[25%] left-[70%] md:size-[700px] size-[200px] bg-sky-500 rounded-full opacity-20 blur-3xl z-[1]"></div>
             <h2 className="md:text-3xl text-xl font-bold mb-8">
-              What Arkos Do?{" "}
+              What Does Arkos Do?{" "}
               <span className="text-gray-400 text-base md:text-lg font-normal">
                 Provides a Set Of Tools To Allow Developers To Skip Standardized
                 RESTful API Development Tasks
@@ -176,9 +152,6 @@ export default function Home() {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6 w-full max-w-6xl">
               <div></div>
               <div className="flex items-center justify-center  rounded-lg flex-col p-2 md:p-4  outline-1 outline-none relative mx-auto w-full text-center">
-                {/* <div className="text-zinc-200 text-lg md:text-2xl font-bold flex gap-2 items-center  w-full text-center justify-center">
-                  POWERED BY
-                </div> */}
                 <div>
                   <img
                     src="/img/arkos-js-logo-dark.svg"
@@ -238,61 +211,13 @@ export default function Home() {
                   NodeJs ↗
                 </div>
                 <p className="text-zinc-400 text-sm">
-                  A strongly typed programming language that builds on
-                  JavaScript, giving you better tooling at any scale.
+                  JavaScript runtime environment that lets developers create
+                  servers, web apps, command line tools and scripts{" "}
                 </p>
               </div>
             </div>
           </div>
         </div>
-
-        {/* <div className="bg-red-500 p-4"></div> */}
-
-        {/* Engines Section */}
-        {/* <section className="bg-[#041633] text-white py-16">
-        <div className="container mx-auto px-4">
-          <h2 className="text-2xl font-bold mb-12">Trusted Developers In</h2>
-
-          <div className="flex justify-center space-x-16 mb-16">
-            <img
-              src="/img/grupo-virgili.png"
-              alt="Grupo Virgili"
-              className="h-12"
-            />
-            <img
-              src="/img/grupo-mesquita.png"
-              alt="Grupo Mesquita"
-              className="h-12"
-            />
-          </div>
-
-          <div className="relative max-w-4xl mx-auto mb-16">
-            <div className="bg-gray-800 text-white py-3 px-8 rounded-md text-center font-bold absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-              ENGINES
-            </div>
-            <div className="border border-gray-600 rounded-md p-12 pt-16 flex justify-center">
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                <div className="bg-black bg-opacity-80 rounded-lg p-6 text-center">
-                  <h3 className="text-xl font-semibold mb-2">nodejs - logo</h3>
-                  <p className="text-gray-400">and description</p>
-                </div>
-
-                <div className="bg-black bg-opacity-80 rounded-lg p-6 text-center">
-                  <h3 className="text-xl font-semibold mb-2">express - logo</h3>
-                  <p className="text-gray-400">and description</p>
-                </div>
-
-                <div className="bg-black bg-opacity-80 rounded-lg p-6 text-center">
-                  <h3 className="text-xl font-semibold mb-2">prisma - logo</h3>
-                  <p className="text-gray-400">and description</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      */}
       </Layout>
     </div>
   );
