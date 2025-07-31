19279af7550c4f34dfe835b672d84150d6955de9
uanela <uanelaluiswayne@gmail.com>
2025-07-31 12:17:39 +0200

    wip

--- packages/create-arkos/src/utils/helpers/npm.helpers.ts
+++ packages/create-arkos/src/utils/helpers/npm.helpers.ts
@@ -10,3 +10,24 @@ export function getNpmPackageVersion(name: string = "arkos"): string {
   const version = execSync(`npm view ${name} version`).toString().trim();
   return version;
 }
+
+/**
+ * Helps getting the current package manager from user agent
+ *
+ * @returns {string} the package manager
+ * @default "npm"
+ */
+export function detectPackageManagerFromUserAgent(): string {
+  const userAgent = process.env.npm_config_user_agent || "";
+
+  if (!userAgent) return "npm";
+  if (userAgent.includes("pnpm")) return "pnpm";
+  if (userAgent.includes("yarn")) return "yarn";
+  if (userAgent.includes("npm")) return "npm";
+  if (userAgent.includes("bun")) return "bun";
+  if (userAgent.includes("cnpm")) return "cnpm";
+  if (userAgent.includes("corepack")) return "corepack";
+  if (userAgent.includes("deno")) return "deno";
+
+  return "npm";
+}
