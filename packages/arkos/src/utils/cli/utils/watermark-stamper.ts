53883854dde907ffe5378aff792d8793bf7f3f32
uanela <uanelaluiswayne@gmail.com>
2025-11-07 12:41:57 +0200

    chore: binding by default to 0.0.0.0 and showing network ip, when in dev

--- packages/arkos/src/utils/cli/utils/watermark-stamper.ts
+++ packages/arkos/src/utils/cli/utils/watermark-stamper.ts
@@ -1,3 +1,4 @@
+import portAndHostAllocator from "../../features/port-and-host-allocator";
 import { fullCleanCwd } from "../../helpers/fs.helpers";
 import { getVersion } from "./cli.helpers";
 
@@ -18,7 +19,15 @@ class WatermarkStamper {
     port: string | undefined;
   }) {
     console.info(`\n  \x1b[1m\x1b[36m  Arkos.js ${getVersion()}\x1b[0m`);
-    if (host && port) console.info(`  - Local:        http://${host}:${port}`);
+    if (host && port) {
+      console.info(
+        `  - Local:        http://${["0.0.0.0", "127.0.0.1"].includes(host) ? "localhost" : host}:${port}`
+      );
+      let nonLocalIp = portAndHostAllocator.getFirstNonLocalIp();
+      if (nonLocalIp && host === "0.0.0.0")
+        console.info(`  - Network:      http://${nonLocalIp}:${port}`);
+    }
+
     if (envFiles?.length || 0 > 1)
       console.info(
         `  - Environments: ${fullCleanCwd(envFiles?.join(", ") || "")
