48c5c3354d9f243832d8dd0f81d91943f83ad35f
uanela <uanelaluiswayne@gmail.com>
2026-01-03 02:02:47 +0200

    chore: bumping up tsxs and arkos versions in sandbox

--- sandbox/arkos/file-upload-window-debug/src/app.ts
+++ sandbox/arkos/file-upload-window-debug/src/app.ts
@@ -2,7 +2,7 @@ import arkos from "arkos";
 
 // console.log(process.env.MY_VAR);
 arkos.init({
-  a,
+  // a,
   // configureApp: async (app) => {
   //   app.use((req, res, next) => {
   //     console.log(req);
