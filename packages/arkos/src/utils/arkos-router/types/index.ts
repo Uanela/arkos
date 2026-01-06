62ca8907ddef3c047e257600ab3e04f6daeb283f
uanela <uanelaluiswayne@gmail.com>
2026-01-06 13:40:24 +0200

    feat(#130): allow wilcard to mean all roles

--- packages/arkos/src/utils/arkos-router/types/index.ts
+++ packages/arkos/src/utils/arkos-router/types/index.ts
@@ -2,7 +2,11 @@ import { IRouter } from "express";
 import { ZodSchema } from "zod";
 import { Options as RateLimitOptions } from "express-rate-limit";
 import { Options as QueryParserOptions } from "../../../utils/helpers/query-parser.helpers";
-import { DetailedAccessControlRule } from "../../../types/auth";
+import {
+  AccessControlConfig,
+  AccessControlRules,
+  DetailedAccessControlRule,
+} from "../../../types/auth";
 import { ArkosErrorRequestHandler, ArkosRequestHandler } from "../../../types";
 import compression from "compression";
 import { OpenApiConfig } from "./openapi-config";
@@ -99,7 +103,7 @@ export type ArkosRouteConfig = {
     | {
         resource: string;
         action: string;
-        rule?: DetailedAccessControlRule | string[];
+        rule?: AccessControlConfig;
       };
   /**
    * Request validation configuration using Zod schemas or class constructors.
