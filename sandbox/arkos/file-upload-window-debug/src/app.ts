1597d99c16bc5a734017237add2b08ffa454166d
uanela <uanelaluiswayne@gmail.com>
2026-01-08 15:58:12 +0200

    fix: remove manual restart on dev

--- sandbox/arkos/file-upload-window-debug/src/app.ts
+++ sandbox/arkos/file-upload-window-debug/src/app.ts
@@ -1,13 +1,39 @@
 import arkos from "arkos";
+import { getHandlerErrors, initializeASTs } from "./utils/ast-handler";
+import { AppError } from "arkos/error-handler";
 
-// console.log(process.env.MY_VAR);
-arkos.init({
-  // a,
-  // configureApp: async (app) => {
-  //   app.use((req, res, next) => {
-  //     console.log(req);
-  //     next();
-  //   });
-  //   return app;
-  // },
-});
+(async () => {
+  console.time("ast");
+  await initializeASTs();
+  console.timeEnd("ast");
+  const login = new AppError("Special One", 408);
+
+  function func(req: any) {
+    if (!req?.user) throw new AppError("Unauthorized", 401);
+
+    throw login;
+  }
+  async function myHandler(req: any, res: any) {
+    func(req);
+    const msg = req == 1 ? "hello" : "buwy";
+
+    throw login;
+    if (!req.body) throw new AppError("hello" + msg + "boss", 400);
+  }
+
+  const errors = getHandlerErrors(myHandler);
+  // console.log(errors);
+  console.log(new Date().toISOString());
+
+  // console.log(process.env.MY_VAR);
+  arkos.init({
+    a,
+    // configureApp: async (app) => {
+    //   app.use((req, res, next) => {
+    //     console.log(req);
+    //     next();
+    //   });
+    //   return app;
+    // },
+  });
+})();
