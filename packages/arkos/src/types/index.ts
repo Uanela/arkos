395d4f75b78548495395c70047283ef2aee4e2db
uanela <uanelaluiswayne@gmail.com>
2026-01-27 09:02:50 +0200

    feat(#82): correctly type req.user based on @prisma/client

--- packages/arkos/src/types/index.ts
+++ packages/arkos/src/types/index.ts
@@ -1,10 +1,10 @@
 import {
   ErrorRequestHandler,
   NextFunction,
-  Request,
   RequestHandler,
   Response,
 } from "express";
+import { ArkosRequest, User } from "./arkos-request";
 
 export type PrismaOperations = "findMany";
 
@@ -204,86 +204,7 @@ export interface AuthPermission {
   role: AuthRole;
 }
 
-export interface User extends Record<string, any> {
-  id: string;
-  isSuperUser: boolean;
-  password: string;
-  passwordChangedAt?: Date;
-  deletedSelfAccountAt: Date;
-  isActive: boolean;
-}
-
-export interface ArkosRequest<
-  P extends Record<string, any> = any,
-  ResBody = any,
-  ReqBody = any,
-  Query extends Record<string, any> = any,
-> extends Request<P, ResBody, ReqBody, Query> {
-  /**
-   * Authenticated user with additional fields
-   */
-  user?: User & Record<string, any>;
-
-  /**
-   * Fields to include in relational queries
-   */
-  relationFields?: Record<string, boolean>;
-
-  /**
-   * Prisma include options for related data
-   */
-  include?: Record<string, any>;
-
-  /**
-   * Data to be sent in the response
-   */
-  responseData?: Record<string, any> | null;
-
-  /**
-   * Additional context data
-   */
-  additionalData?: Record<string, any> | null;
-
-  /**
-   * HTTP status code for the response
-   */
-  responseStatus?: number;
-
-  /**
-   * Typed request body
-   */
-  body: ReqBody;
-
-  /**
-   * Prisma query options (where, orderBy, select, etc.)
-   */
-  prismaQueryOptions?: Record<string, any>;
-
-  /**
-   * Typed query parameters
-   */
-  query: Query;
-
-  /**
-   * JWT token used in authentication process
-   */
-  accessToken?: string;
-
-  /**
-   * Query parameters after being handled and transformed by middleware
-   */
-  transformedQuery?: Record<string, any>;
-
-  /**
-   * Processed filters from APIFeatures.filters
-   */
-  filters?: Record<string, any>;
-
-  /**
-   * Name of the Prisma model being queried
-   */
-  modelName?: string;
-}
+export { ArkosRequest, User };
 
 export interface ArkosResponse<
   ResBody = any,
