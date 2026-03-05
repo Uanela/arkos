029185ce525628db420dcaadebac857964132160
uanela <uanelaluiswayne@gmail.com>
2026-03-05 11:29:59 +0200

    chore: update sandbox to test last changes

--- sandbox/arkos/file-upload-window-debug/src/modules/post/post.controller.ts
+++ sandbox/arkos/file-upload-window-debug/src/modules/post/post.controller.ts
@@ -1,6 +1,11 @@
+import { ArkosRequest } from "arkos";
 import { BaseController } from "arkos/controllers";
-  
-class PostController extends BaseController {}
+
+class PostController extends BaseController {
+  async my(req: ArkosRequest) {
+    req.user;
+  }
+}
 
 const postController = new PostController("post");
 
