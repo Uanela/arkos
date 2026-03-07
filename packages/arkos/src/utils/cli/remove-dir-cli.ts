06e2fcc135cb671a1f6d35612cd568070f60a195
uanela <uanelaluiswayne@gmail.com>
2026-03-07 17:18:51 +0200

    fix ts types build process

--- packages/arkos/src/utils/cli/remove-dir-cli.ts
+++
@@ -1,8 +0,0 @@
-import { getFolderPathFromArgs, isEntryPoint, removeDir } from "../remove-dir";
-
-(async () => {
-  if (isEntryPoint()) {
-    const folderPath = getFolderPathFromArgs();
-    removeDir(folderPath);
-  }
-})();
