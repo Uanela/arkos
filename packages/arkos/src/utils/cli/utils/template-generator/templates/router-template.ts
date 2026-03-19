3a92a54447b481f0f04ddb0c69f534917f757387
uanela <uanelaluiswayne@gmail.com>
2026-03-20 00:03:16 +0200

    feat(arkos): improve generated router template with openapi tags and special case handling for auth and file-upload

--- packages/arkos/src/utils/cli/utils/template-generator/templates/router-template.ts
+++ packages/arkos/src/utils/cli/utils/template-generator/templates/router-template.ts
@@ -1,5 +1,6 @@
-import pluralize from "pluralize";
+import pluralize, { singular } from "pluralize";
 import { TemplateOptions } from "../../template-generators";
+import { pascalCase } from "../../../../helpers/change-case.helpers";
 
 export function generateRouterTemplate(options: TemplateOptions): string {
   const { modelName } = options;
@@ -7,9 +8,12 @@ export function generateRouterTemplate(options: TemplateOptions): string {
   if (!modelName)
     throw new Error("Module name is required for router template");
 
-  return `import { ArkosRouter } from 'arkos'
+  return `import { ArkosRouter } from 'arkos';${modelName.kebab === "file-upload" ? "\nimport config from '@/arkos.config'" : ""}
 
-const ${modelName.camel}Router = ArkosRouter({ prefix: "${pluralize(modelName.kebab)}" })
+const ${modelName.camel}Router = ArkosRouter({ 
+prefix: "${modelName.kebab === "auth" ? "auth" : modelName.kebab === "file-upload" ? `config?.fileUpload?.baseUploadRoute!` : pluralize(modelName.kebab)}",
+  openapi: { tags: ["${singular(pascalCase(modelName.kebab.replaceAll("-", " ")))}"] }
+})
 
 export default ${modelName.camel}Router
 `;
