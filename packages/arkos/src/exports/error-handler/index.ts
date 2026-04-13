210d0fa6425ab60f4ad4589046ed81a576d02e07
uanela <uanelaluiswayne@gmail.com>
2026-04-13 11:30:45 +0200

    feat(arkos): adding default error classes

--- packages/arkos/src/exports/error-handler/index.ts
+++ packages/arkos/src/exports/error-handler/index.ts
@@ -1,4 +1,5 @@
 import catchAsync from "../../modules/error-handler/utils/catch-async";
 import AppError from "../../modules/error-handler/utils/app-error";
 
+export * from "../../modules/error-handler/utils/errors";
 export { catchAsync, AppError };
