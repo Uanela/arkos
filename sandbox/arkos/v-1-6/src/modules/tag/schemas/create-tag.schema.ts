93cbf4757c9d4ff5ba45a19427c4003987e81b47
uanela <uanelaluiswayne@gmail.com>
2026-04-29 08:02:36 +0200

    remove unneeded files and update arkos logo for readme

--- sandbox/arkos/v-1-6/src/modules/tag/schemas/create-tag.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-const CreateTagSchema = z.object({
-
-});
-
-export default CreateTagSchema;
-
-export type CreateTagSchemaType = z.infer<typeof CreateTagSchema>;
--- sandbox/arkos/v-1-6/src/modules/tag/tag.controller.ts
+++
@@ -1,7 +0,0 @@
-import { BaseController } from "arkos/controllers";
-  
-export class TagController extends BaseController {}
-
-const tagController = new TagController("tag");
-
-export default tagController;
