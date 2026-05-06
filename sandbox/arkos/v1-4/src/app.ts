c4f0d3cfa28299e9c61fd74a1bc96ac93a0abe3e
uanela <uanelaluiswayne@gmail.com>
2026-05-06 10:28:50 +0200

    chore(sandbox): update v1-4 arkos version and add upload test endpoint

--- sandbox/arkos/v1-4/src/app.ts
+++ sandbox/arkos/v1-4/src/app.ts
@@ -1,6 +1,27 @@
-import arkos from "arkos";
+import arkos, { ArkosRequest, ArkosRouter } from "arkos";
 import userRouter from "./modules/user/user.router";
 
+const router = ArkosRouter();
+
+router.post(
+  {
+    path: "/test-upload-single-one-level",
+    // validation: { body: z.object({}) },
+    experimental: {
+      uploads: {
+        type: "array",
+        field: "banner",
+      },
+    },
+  },
+  (req: ArkosRequest, res) => {
+    console.log("body", req.body);
+    console.log("file", req.file);
+    console.log("files", req.files);
+    res.send("pong");
+  }
+);
+
 arkos.init({
-  use: [userRouter], // pass your additional middlewares/routers here
+  use: [userRouter, router], // pass your additional middlewares/routers here
 });
