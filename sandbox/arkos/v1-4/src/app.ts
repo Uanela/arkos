1640fc53549c701e5dbb310e1d24f7095b43c01e
uanela <uanelaluiswayne@gmail.com>
2026-05-06 13:22:19 +0200

    test(sandbox): add test route with upload and custom OpenAPI in v1-4

--- sandbox/arkos/v1-4/src/app.ts
+++ sandbox/arkos/v1-4/src/app.ts
@@ -1,6 +1,25 @@
-import arkos from "arkos";
+import arkos, { ArkosRouter } from "arkos";
 import userRouter from "./modules/user/user.router";
+import z from "zod";
+
+const router = ArkosRouter();
+
+router.post(
+  {
+    path: "/testing",
+    experimental: {
+      uploads: { type: "single", field: "name" },
+      openapi: {
+        requestBody: z.object({ big: z.string() }),
+        responses: { 200: z.object({ big: z.string() }) },
+      },
+    },
+  },
+  (req, res) => {
+    res.json({ ping: "pong" });
+  }
+);
 
 arkos.init({
-  use: [userRouter], // pass your additional middlewares/routers here
+  use: [userRouter, router], // pass your additional middlewares/routers here
 });
