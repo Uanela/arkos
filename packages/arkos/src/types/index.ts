f0e8ef5a8bdf997a079b8e884e1b2492875e7e7f
uanela <uanelaluiswayne@gmail.com>
2026-05-07 08:02:45 +0200

    feat(arkos): make req.user nullable

--- packages/arkos/src/types/index.ts
+++ packages/arkos/src/types/index.ts
@@ -200,7 +200,7 @@ export interface ArkosRequest<
   /**
    * Authenticated user
    */
-  user?: User;
+  user?: User | null;
   /**
    * Single uploaded file, populated when using `multer.single()`
    */
