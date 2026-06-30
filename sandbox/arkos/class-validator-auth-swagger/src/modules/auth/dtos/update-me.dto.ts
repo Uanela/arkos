322e6b6f3ddb1649cdccdd8483c9b6781b9cbe0e
uanela <uanelaluiswayne@gmail.com>
2026-06-30 11:20:32 +0200

    chore(arkos): fix upload manager test and add dedicate 1.5 sandbox project

--- sandbox/arkos/class-validator-auth-swagger/src/modules/auth/dtos/update-me.dto.ts
+++
@@ -1,9 +0,0 @@
-import 'reflect-metadata'
-import { IsString, IsNotEmpty, IsOptional } from "class-validator";
-
-export default class UpdateMeDto {
-  @IsString()
-  @IsNotEmpty({ message: 'email is required' })
-  @IsOptional()
-  email!: string
-}
