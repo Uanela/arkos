1c8a198a405236e7e472d19f93ff0325a1608007
Uanela <uanelaluiswayne@gmail.com>
2026-07-27 23:01:49 +0200

    refactor(utils): update utility exports

--- packages/arkos/src/utils/validate-schema.ts
+++ packages/arkos/src/utils/validate-schema.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 import { getArkosConfig } from "./helpers/arkos-config.helpers";
-import deepmerge from "./helpers/deepmerge.helper";
 
 export type ZodValidationOptions = { forbidNonWhitelisted?: boolean };
 /**
