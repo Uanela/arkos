35bb6aaa407f4ca5f00d91efe4c55eaee0b225f5
Uanela <uanelaluiswayne@gmail.com>
2026-08-03 14:42:20 +0200

    chore: update sandbox

--- sandbox/arkos/canary-v2/src/modules/user/user.service.ts
+++ sandbox/arkos/canary-v2/src/modules/user/user.service.ts
@@ -1,15 +1,18 @@
 import { ArkosPrismaService } from "arkos/services";
 import { Prisma } from "@prisma/client";
 import { CreateUserSchema } from "./schemas/create-user.schema";
+import prisma from '@/src/utils/prisma';
 
 class UserService extends ArkosPrismaService<"user"> {
   async createOne<O extends Omit<Prisma.UserCreateArgs, "data">>(data: CreateUserSchema, options?: O) {
 
-    console.log("running", data, options)
+    console.log("running", data, options);
 
-    const result = await super.createOne(data, options)
+    const result = await super.createOne(data);
+    // const result2 = await prisma.user.create({ data: { ...data } });
+    // return true;
 
-    return result
+    return result;
   }
 }
 
